0c0ee932af8d6c5053ef5b61fbed8ff66b58e02f
HoThienNhan1997 <hothiennhan1997@gmail.com>
2019-05-22 18:44:41 +0700

    Updates

---
+++ precache-manifest.c55a57d9e10f5c5e171b3b5b7368fda5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "588446246f8f016f56114ad77221cb01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "800ac71906030ccfd631",
+    "url": "/static/css/main.b66eb680.chunk.css"
+  },
+  {
+    "revision": "7deb8b333a4d9ccdfed9",
+    "url": "/static/js/2.7e8f43ee.chunk.js"
+  },
+  {
+    "revision": "800ac71906030ccfd631",
+    "url": "/static/js/main.6d338f71.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
